474edfa06c5eb032e4dbb3c52b22fe6d921892f4
sunilaltimetrik <sonisunil486@gmail.com>
2016-05-03 10:42:05 +0530

    Update curd2.js

--- curd2.js
+++ curd2.js
@@ -4,23 +4,42 @@ app.controller('carController',function($scope){
 $scope.viewdisplay= false;
    	$scope.carDetail=[];
 	$scope.addCar= function(){
-		$scope.carDetail.push({name:$scope.cName, type:$scope.cType, model:$scope.cModel});
-        $scope.cName = '';
-        $scope.cType = '';
-		$scope.cModel = '';
+        if (!$scope.cName)
+                {
+                    alert("Enter Car Name");
+                }
+                else if (!$scope.cType)
+                {
+                    alert("Enter Type");
+                }
+                else if (!$scope.cModel) {
+                    alert("Enter Model");
+                }
+                else {
+		              $scope.carDetail.push({name:$scope.cName, type:$scope.cType, model:$scope.cModel});
+                      $scope.cName = '';
+                      $scope.cType = '';
+		              $scope.cModel = '';
+                }
 	}
     $scope.view= function(index){
 		$scope.data=$scope.carDetail[index];
 		$scope.viewdisplay= true;
 	}
-$scope.deletes = function(index) {
-$scope.carDetail.splice(index,1);
-}
-$scope.edit = function(rowdata,upindex) {
-                $scope.cName = rowdata.name;
-                $scope.cType = rowdata.type;
-                $scope.cModel=rowdata.model;
-                $scope.carDetail[upindex] = {name:$scope.cName, type:$scope.cType, model:$scope.cModel};
+    $scope.deletes = function(index) {
+    $scope.carDetail.splice(index,1);
+    $scope.data= false;
+    }
+    $scope.edit = function(updateData,upindex) {
+                $scope.cName = updateData.name;
+                $scope.cType = updateData.type;
+                $scope.cModel=updateData.model;
 
-}
+                $scope.carDetail[upindex] = {name:$scope.cName, type:$scope.cType, model:$scope.cModel};
+//$scope.carDetail.splice(upindex, 1, $scope.cName,)
+//$scope.deletes(upindex);
+
+  }
 });
+
+
